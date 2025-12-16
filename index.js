618740780299b3c8b76747aca80c2ff325de0994
meagle-pixel <m.eagle@hotmail.fr>
2025-12-16 16:49:09 +0100

    Ajout d'alertes sur le formulaire avec JavaScript

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
+const planets = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune",
+];
 
-const API_KEY = "512de027-801b-4399-a599-e5434caff044";
+planets.forEach((name) => {
+  fetch(`https://api.api-ninjas.com/v1/planets?name=${name}`, {
+    headers: {
+      "X-Api-Key": "AR8OsTVJdP3da+XsLsRQ1A==dS0QmDvtepXP5xO2",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
 
-async function loadPlanets() {
-  try {
-    const response = await fetch(
-      "https://api.le-systeme-solaire.net/rest/bodies?filter[]=isPlanet,eq,true",
-      {
-        headers: {
-          Authorization: `Bearer ${API_KEY}`
-        }
-      }
-    );
+      const planet = data[0];
+      const container = document.getElementById("planet-info");
+      container.innerHTML += `
+            <h2>${planet.name}</h2>
+            <p>Mass: ${planet.mass}</p>
+            <p>Radius: ${planet.radius}</p>
+            <p>Period: ${planet.period}</p>
+            <p>Semi-major axis: ${planet.semi_major_axis}</p>
+            <hr>
+        `;
+    });
+});
 
-    const data = await response.json();
-    console.log(data); // ← ici tu verras tout le JSON
-  } catch (err) {
-    console.error("Erreur API :", err);
+// FORMULAIRE
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const lastName = document.getElementById("lastName").value.trim();
+  const firstName = document.getElementById("firstName").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const mobile = document.getElementById("mobile").value.trim();
+  const message = document.getElementById("textarea").value.trim();
+
+  if (!lastName || !firstName || !email || !message) {
+    alert("Veuillez remplir tous les champs obligatoires !");
+    return;
   }
-}
 
-loadPlanets();
+  console.log({
+    lastName, firstName, email, mobile, message
+  });
+
+  alert("Formulaire complété avec succés !")
+  form.reset ();
+});
+getElementById
