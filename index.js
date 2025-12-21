62f0e51554052ea3a8ecbb642529b75ec7b5ce98
BlazeAttitude <m.eagle@hotmail.fr>
2025-12-21 20:46:20 +0100

    test de la branche 1

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 
-const planetes = ["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune"];
+// const planetes = ["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune"];
 
 
-async function afficherPlanetes() {
-  const apiUrl = "https://api.api-ninjas.com/v1/planets?name=earth";
-  const apiKey = "AR8OsTVJdP3da+XsLsRQ1A==dS0QmDvtepXP5xO2";
+// async function afficherPlanetes() {
+//   const apiUrl = "https://api.api-ninjas.com/v1/planets?name=earth";
+//   const apiKey = "AR8OsTVJdP3da+XsLsRQ1A==dS0QmDvtepXP5xO2";
 
-try {
-  const response = await fetch(apiUrl, {
-    headers: { 
-      'X-Api-Key': apiKey
-  }
- });
+// try {
+//   const response = await fetch(apiUrl, {
+//     headers: { 
+//       'X-Api-Key': apiKey
+//   }
+//  });
 
-  const data = await response.json();
-  console.log("Planètes reçues :", data);
+//   const data = await response.json();
+//   console.log("Planètes reçues :", data);
 
-  // Dans le but de l'afficher dans le HTML
+//   // Dans le but de l'afficher dans le HTML
 
-  const container = document.getElementById("planet-info");
+//   const container = document.getElementById("planet-info");
 
-  data.forEach(planete => {
-    const p = document.createElement("p");
-    p.textContent = planete.name;
-    container.appendChild(p);
-  });
+//   data.forEach(planete => {
+//     const p = document.createElement("p");
+//     p.textContent = planete.name;
+//     container.appendChild(p);
+//   });
 
-} catch (error) {
-  console.error("Erreur", error);
-  alert("Impossible de récupérer les données");
-}
-}
-afficherPlanetes();
+// } catch (error) {
+//   console.error("Erreur", error);
+//   alert("Impossible de récupérer les données");
+// }
+// }
+// afficherPlanetes();
 
-// FORMULAIRE
+// ********** FORMULAIRE **********
 
 const form = document.querySelector("form");
 
